e8cd5562d973245f46ae47129d1dda0453ef8626
Djordje Vlaisavljevic <dzvlais@gmail.com>
2022-08-18 17:41:08 +0200

    Turned on the flag

--- src/components/common/ProductsSection.js
+++ src/components/common/ProductsSection.js
@@ -514,7 +514,7 @@ export const ProductsSectionStyles = ({site}) => {
     `;
 };
 
-const freeTrialFlag = 0;
+const freeTrialFlag = 1;
 const ProductsContext = React.createContext({
     selectedInterval: 'month',
     selectedProduct: 'free',
